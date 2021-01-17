05fe260ce8814e4b12166e96a58fde391a4f6886
cubae <shinycloudbae@gmail.com>
2021-01-17 18:19:14 +0900

    Initial commit

---
+++ src/main/java/cloudship/webservice/Application.java
@@ -0,0 +1,15 @@
+package cloudship.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        run(Application.class, args);
+    }
+}
